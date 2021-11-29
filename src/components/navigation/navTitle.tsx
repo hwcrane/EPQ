cd3a933cf4524dd86c1ed48325c5777e72a88598
Harry Crane <37228043+hwcrane@users.noreply.github.com>
2021-11-29 00:28:24 +0000

    Update navTitle.tsx

--- src/components/navigation/navTitle.tsx
+++ src/components/navigation/navTitle.tsx
@@ -4,6 +4,6 @@ import "./navBar.css";
 // component for the navbar title
 export default class NavTitle extends React.Component {
     render() {
-        return <div className="navItem navTitle">Algorithm Visuliser</div>;
+        return <div className="navItem navTitle">Algorithm Visualiser</div>;
     }
 }
